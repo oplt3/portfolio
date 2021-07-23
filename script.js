f02d251aa526e6980ebb1a2b1f63df5b875b2148
oplt3 <85621168+oplt3@users.noreply.github.com>
2021-07-23 21:05:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
   modal.style.display = "none";
 }
+
+
+ /** Hidden vertical bar when the user clicks on a link*/
+const aListVertical = document.querySelectorAll(".ham-menu a");
+
+aListVertical.forEach(function (aElement) {
+  aElement.addEventListener('click', () => {
+    const hamMenu = document.querySelector("#ham-menu");
+    hamMenu.checked = false;
+
+  })
+});
+
+// If the screen has a width above 1000px, the menu mustn't stay open //
+
+// const size = window.innerWidth;
+// window.onresize = function (){
+//   if (size>=1000){
+//     const hamMenu = document.querySelector("div\.ham-menu");
+//     console.log(hamMenu.hidden)
+//     hamMenu.hidden = true;
+//     console.log(hamMenu.hidden)
+//   }
+// }
+
